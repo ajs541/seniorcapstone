31b30dd7ef2646e59a2f6e32e0836af5c5a1e895
ajs541 <ajstand@live.com>
2024-08-26 14:23:40 -0500

    Added more images, grabbed an example of complex buttons for MUI. Having issues loading in local images but web url's seem to work fine.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Button from './Button.js';
+import ComplexButton from './ComplexButton.js';
 
 function App() {
   return (
     <div className="App">
+      <ComplexButton />
       <Button example="Hello World"/>
       <p>This device is a {navigator.userAgent}</p>
     </div>
