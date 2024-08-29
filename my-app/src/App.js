fcd3435dd5a80f934dc74ec7bd9bc0f2a019003d
ajs541 <ajstand@live.com>
2024-08-28 19:31:08 -0500

    Added python/SQL database

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Button from './components/Button.js';
 import ComplexButton from './components/ComplexButton.js';
+import GradeButton from './components/GradeButtons.js';
+import LinkButton from './components/LinkButton.js';
+import RealButtonTest from './components/RealButtonTest.js';
+import K_2 from './K_2.js';
 
 function App() {
   return (
     <div className="App">
-      <ComplexButton />
+      
+      <RealButtonTest link = './K_2.js' />
+      {/*
+      <GradeButton />
+      <LinkButton />
+      <ComplexButton /> */}
+      {/*      
       <Button example="Hello World"/>
       <p>This device is a {navigator.userAgent}</p>
+       */}
     </div>
   );
 }
