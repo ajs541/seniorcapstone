4fa3cdda897f76c48cf3d084b3810d3c6868e630
ajs541 <ajstand@live.com>
2024-09-05 10:38:55 -0500

    Added FetchFromAPI.  First draft of API (will make more efficient down the line)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -28,13 +28,20 @@ const NotFound = () => (
 const Home = () => (
   <BackgroundElement
     title="Welcome to WebSafari!"
-    subtitle="Click the habitats button to start."
+    subtitle="Click your grade level to start."
     link="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Indore_zoo_gate.jpg/1280px-Indore_zoo_gate.jpg"
   >
-    <Box>
-      <Button component={Link} to="/Habitats" variant="contained">
-        Habitats
-      </Button>
+    <Box sx={{display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%', justifyContent: "space-around", alignItems: 'center', }}>
+      <Box>
+        <Button component={Link} to="/Habitats" variant="contained">
+          K-2
+        </Button>
+      </Box>
+      <Box>
+        <Button component={Link} to="/Habitats" variant="contained">
+          3-6
+        </Button>
+      </Box>
     </Box>
   </BackgroundElement>
 )
@@ -42,7 +49,7 @@ const Home = () => (
 const Habitats = () => (
   <BackgroundElement
     title="Choose a habitat."
-    link="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Indore_zoo_gate.jpg/1280px-Indore_zoo_gate.jpg"
+    link="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/View_over_the_garden_square_Artisplein_with_bird_cage%3B_free_photo_Amsterdam%2C_Fons_Heijnsbroek%2C_10-2022.jpg/1280px-View_over_the_garden_square_Artisplein_with_bird_cage%3B_free_photo_Amsterdam%2C_Fons_Heijnsbroek%2C_10-2022.jpg"
   >
     <p>Testing page</p>
   </BackgroundElement>
