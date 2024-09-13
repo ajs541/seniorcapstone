005910e2f9e027634ac11fb69024c7c54784061e
ajs541 <ajstand@live.com>
2024-09-13 10:40:54 -0500

    add recursive install, add guardrails, and fix poetry install

--- Python/create_db.py
+++ Python/create_db.py
@@ -1,4 +1,16 @@
 import sqlite3
+import sys
+import os
+
+if sys.argv[1] != 'force':
+    ans = input('Are you sure? This will overwrite zoo.db! [y/N] ')
+
+    if ans.lower() != 'y':
+        print('Exiting...')
+        exit()
+
+if os.path.exists('zoo.sqlite'):
+    os.remove('zoo.sqlite')
 
 con = sqlite3.connect("zoo.sqlite")
 con.execute("PRAGMA foreign_keys = ON")
